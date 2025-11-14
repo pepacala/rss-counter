df18f6622ce2669a77961f8ae4d570a6534b5066
pepacala <pepacala@users.noreply.github.com>
2025-11-14 00:07:25 +0000

    deploy: 0ed720778f8a48b86c0e301a66a6321f3dd0e0de

---
+++ assets/index-Bt_xFlcA.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-CHsasOJn.js";const t=function(){return console.log("Pending Records"),n.jsx("h2",{className:"text-4xl p-10",children:"Pending"})};export{t as pendingComponent};
